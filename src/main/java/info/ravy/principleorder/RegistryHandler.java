9e0d691a70cd7370082d2fd1038ee64c2b6281a7
BurningEmbyr <BurningEmbyr@users.noreply.github.com>
2022-02-25 17:37:20 -0500

    Up to create method, but not past it

--- src/main/java/info/ravy/principleorder/RegistryHandler.java
+++ src/main/java/info/ravy/principleorder/RegistryHandler.java
@@ -1,8 +1,12 @@
 package info.ravy.principleorder;
 
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroup.*;
+import net.minecraftforge.common.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,12 +15,24 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler {
     // create DeferredRegister object
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PrincipleOrder.MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, PrincipleOrder.MODID);
 
     public static void init() {
         // attach DeferredRegister to the event bus
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
+    // register copper ore block
+    public static final RegistryObject<Block> COPPER_ORE = BLOCKS.register("copper_ore", () ->
+        new Block(
+                AbstractBlock.Properties.create(Material.STONE)
+                .hardnessAndResistance(5.0f, 6.0f)
+                .sound(SoundType.STONE)
+                .harvestLevel(1)
+                .harvestTool(ToolType.PICKAXE)
+        )
+    );
     // register item
     public static final RegistryObject<Item> COPPER = ITEMS.register("copper", () ->
             new Item(
